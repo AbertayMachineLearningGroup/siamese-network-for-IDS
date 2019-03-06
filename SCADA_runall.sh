ab0da632d8222f3b05ca74b5375509ef20d45053
Hanan <hanan.hendy@gmail.com>
2019-03-06 13:16:00 +0000

    Scripts to run 5-fold cross validation on all architectures

---
+++ SCADA_runall.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/SCADA_dataset_processed.csv' --dataset_name SCADA --output no_window_SCADA_rerun_Net0.csv --network_id 'SCADA_0' --k 0
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/SCADA_dataset_processed.csv' --dataset_name SCADA --output no_window_SCADA_rerun_Net0.csv --network_id 'SCADA_0' --k 1
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/SCADA_dataset_processed.csv' --dataset_name SCADA --output no_window_SCADA_rerun_Net0.csv --network_id 'SCADA_0' --k 2
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/SCADA_dataset_processed.csv' --dataset_name SCADA --output no_window_SCADA_rerun_Net0.csv --network_id 'SCADA_0' --k 3
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/SCADA_dataset_processed.csv' --dataset_name SCADA --output no_window_SCADA_rerun_Net0.csv --network_id 'SCADA_0' --k 4
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/SCADA_dataset_processed.csv' --dataset_name SCADA --output no_window_SCADA_rerun_Net1.csv --network_id 'SCADA_1' --k 0
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/SCADA_dataset_processed.csv' --dataset_name SCADA --output no_window_SCADA_rerun_Net1.csv --network_id 'SCADA_1' --k 1
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/SCADA_dataset_processed.csv' --dataset_name SCADA --output no_window_SCADA_rerun_Net1.csv --network_id 'SCADA_1' --k 2
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/SCADA_dataset_processed.csv' --dataset_name SCADA --output no_window_SCADA_rerun_Net1.csv --network_id 'SCADA_1' --k 3
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/SCADA_dataset_processed.csv' --dataset_name SCADA --output no_window_SCADA_rerun_Net1.csv --network_id 'SCADA_1' --k 4
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/SCADA_dataset_processed.csv' --dataset_name SCADA --output no_window_SCADA_rerun_Net2.csv --network_id 'SCADA_2' --k 0
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/SCADA_dataset_processed.csv' --dataset_name SCADA --output no_window_SCADA_rerun_Net2.csv --network_id 'SCADA_2' --k 1
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/SCADA_dataset_processed.csv' --dataset_name SCADA --output no_window_SCADA_rerun_Net2.csv --network_id 'SCADA_2' --k 2
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/SCADA_dataset_processed.csv' --dataset_name SCADA --output no_window_SCADA_rerun_Net2.csv --network_id 'SCADA_2' --k 3
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/SCADA_dataset_processed.csv' --dataset_name SCADA --output no_window_SCADA_rerun_Net2.csv --network_id 'SCADA_2' --k 4
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/SCADA_dataset_processed.csv' --dataset_name SCADA --output no_window_SCADA_rerun_Net3.csv --network_id 'SCADA_3' --k 0
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/SCADA_dataset_processed.csv' --dataset_name SCADA --output no_window_SCADA_rerun_Net3.csv --network_id 'SCADA_3' --k 1
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/SCADA_dataset_processed.csv' --dataset_name SCADA --output no_window_SCADA_rerun_Net3.csv --network_id 'SCADA_3' --k 2
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/SCADA_dataset_processed.csv' --dataset_name SCADA --output no_window_SCADA_rerun_Net3.csv --network_id 'SCADA_3' --k 3
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/SCADA_dataset_processed.csv' --dataset_name SCADA --output no_window_SCADA_rerun_Net3.csv --network_id 'SCADA_3' --k 4
---
+++ STA_runall.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/STA2018_DatasetPreprocessed/' --dataset_name STA --output STA_rerun__Net0.csv --network_id 'STA_A1' --k 0
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/STA2018_DatasetPreprocessed/' --dataset_name STA --output STA_rerun__Net0.csv --network_id 'STA_A1' --k 1
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/STA2018_DatasetPreprocessed/' --dataset_name STA --output STA_rerun__Net0.csv --network_id 'STA_A1' --k 2
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/STA2018_DatasetPreprocessed/' --dataset_name STA --output STA_rerun__Net0.csv --network_id 'STA_A1' --k 3
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/STA2018_DatasetPreprocessed/' --dataset_name STA --output STA_rerun__Net0.csv --network_id 'STA_A1' --k 4
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/STA2018_DatasetPreprocessed/' --dataset_name STA --output STA_rerun__Net1.csv --network_id 'STA_A2' --k 0
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/STA2018_DatasetPreprocessed/' --dataset_name STA --output STA_rerun__Net1.csv --network_id 'STA_A2' --k 1
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/STA2018_DatasetPreprocessed/' --dataset_name STA --output STA_rerun__Net1.csv --network_id 'STA_A2' --k 2
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/STA2018_DatasetPreprocessed/' --dataset_name STA --output STA_rerun__Net1.csv --network_id 'STA_A2' --k 3
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/STA2018_DatasetPreprocessed/' --dataset_name STA --output STA_rerun__Net1.csv --network_id 'STA_A2' --k 4
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/STA2018_DatasetPreprocessed/' --dataset_name STA --output STA_rerun__Net2.csv --network_id 'STA_A3' --k 0
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/STA2018_DatasetPreprocessed/' --dataset_name STA --output STA_rerun__Net2.csv --network_id 'STA_A3' --k 1
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/STA2018_DatasetPreprocessed/' --dataset_name STA --output STA_rerun__Net2.csv --network_id 'STA_A3' --k 2
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/STA2018_DatasetPreprocessed/' --dataset_name STA --output STA_rerun__Net2.csv --network_id 'STA_A3' --k 3
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/STA2018_DatasetPreprocessed/' --dataset_name STA --output STA_rerun__Net2.csv --network_id 'STA_A3' --k 4
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/STA2018_DatasetPreprocessed/' --dataset_name STA --output STA_rerun__Net3.csv --network_id 'STA_A4' --k 0
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/STA2018_DatasetPreprocessed/' --dataset_name STA --output STA_rerun__Net3.csv --network_id 'STA_A4' --k 1
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/STA2018_DatasetPreprocessed/' --dataset_name STA --output STA_rerun__Net3.csv --network_id 'STA_A4' --k 2
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/STA2018_DatasetPreprocessed/' --dataset_name STA --output STA_rerun__Net3.csv --network_id 'STA_A4' --k 3
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/STA2018_DatasetPreprocessed/' --dataset_name STA --output STA_rerun__Net3.csv --network_id 'STA_A4' --k 4
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/STA2018_DatasetPreprocessed/' --dataset_name STA --output STA_rerun__Net4.csv --network_id 'STA_A5' --k 0
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/STA2018_DatasetPreprocessed/' --dataset_name STA --output STA_rerun__Net4.csv --network_id 'STA_A5' --k 1
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/STA2018_DatasetPreprocessed/' --dataset_name STA --output STA_rerun__Net4.csv --network_id 'STA_A5' --k 2
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/STA2018_DatasetPreprocessed/' --dataset_name STA --output STA_rerun__Net4.csv --network_id 'STA_A5' --k 3
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/STA2018_DatasetPreprocessed/' --dataset_name STA --output STA_rerun__Net4.csv --network_id 'STA_A5' --k 4
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/STA2018_DatasetPreprocessed/' --dataset_name STA --output STA_rerun__Net5.csv --network_id 'STA_A6' --k 0
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/STA2018_DatasetPreprocessed/' --dataset_name STA --output STA_rerun__Net5.csv --network_id 'STA_A6' --k 1
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/STA2018_DatasetPreprocessed/' --dataset_name STA --output STA_rerun__Net5.csv --network_id 'STA_A6' --k 2
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/STA2018_DatasetPreprocessed/' --dataset_name STA --output STA_rerun__Net5.csv --network_id 'STA_A6' --k 3
+
+python main.py --verbose F --path '/home/hananhindy/Dropbox/SiameseNetworkDatasetFiles/DatasetProcessedFiles/STA2018_DatasetPreprocessed/' --dataset_name STA --output STA_rerun__Net5.csv --network_id 'STA_A6' --k 4
