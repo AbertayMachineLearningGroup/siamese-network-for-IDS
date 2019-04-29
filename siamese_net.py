7fe8a1ac1c1a6ca3e8bc79bf32c04c4789e231eb
Hanan <hanan.hendy@gmail.com>
2019-04-29 16:11:30 +0100

    Siamese new architecture

--- siamese_net.py
+++ siamese_net.py
@@ -7,10 +7,12 @@ Created on Thu Nov 15 12:19:36 2018
 """
 # based on the implementation in https://github.com/sorenbouma/keras-oneshot
 
-from keras.layers import Input, Dense, Lambda
+from keras.layers import Input, Dense, Lambda, Dropout, BatchNormalization, Activation
 from keras.models import Model, Sequential
 from keras import backend as K
-from keras.optimizers import Adam
+from keras.optimizers import Adam, RMSprop
+from keras.regularizers import l2
+import numpy.random as rng
 
 class SiameseNet:
     def __init__(self, input_shape, network_id = 'kdd_0', verbose = True):
@@ -18,121 +20,71 @@ class SiameseNet:
         self.left_input = Input(input_shape)
         self.right_input = Input(input_shape)
         self.convnet = Sequential()
-
-        if network_id == 'kdd_0':
-            self.convnet.add(Dense(units = 95, kernel_initializer = 'uniform', activation = 'relu', input_shape = input_shape))
-            self.convnet.add(Dense(units = 70, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 47, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 23, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 5, kernel_initializer = 'uniform', activation = 'relu'))
-        elif network_id == 'kdd_1':
-            self.convnet.add(Dense(units = 98, kernel_initializer = 'uniform', activation = 'relu', input_shape = input_shape))
-            self.convnet.add(Dense(units = 79, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 59, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 39, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 20, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 5, kernel_initializer = 'uniform', activation = 'relu'))
-        elif network_id == 'kdd_2':
-            self.convnet.add(Dense(units = 101, kernel_initializer = 'uniform', activation = 'relu', input_shape = input_shape))
-            self.convnet.add(Dense(units = 84, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 67, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 51, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 34, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 17, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 5, kernel_initializer = 'uniform', activation = 'relu'))
-        elif network_id == 'kdd_3':
-            self.convnet.add(Dense(units = 103, kernel_initializer = 'uniform', activation = 'relu', input_shape = input_shape))
-            self.convnet.add(Dense(units = 89, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 74, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 59, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 44, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 30, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 15, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 5, kernel_initializer = 'uniform', activation = 'relu'))       
-        elif network_id == 'STA_0' or network_id == 'STA_A4':
-            self.convnet.add(Dense(units = 510, kernel_initializer = 'uniform', activation = 'relu', input_shape = input_shape))
-            self.convnet.add(Dense(units = 450, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 390, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 330, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 270, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 210, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 150, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 90, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 30, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 3, kernel_initializer = 'uniform', activation = 'relu'))
-        elif network_id == 'STA_1' or network_id == 'STA_A5':
-            self.convnet.add(Dense(units = 480, kernel_initializer = 'uniform', activation = 'relu', input_shape = input_shape))
-            self.convnet.add(Dense(units = 360, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 240, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 120, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 3, kernel_initializer = 'uniform', activation = 'relu'))
-        elif network_id == 'STA_2' or network_id == 'STA_A6':
-            self.convnet.add(Dense(units = 500, kernel_initializer = 'uniform', activation = 'relu', input_shape = input_shape))
-            self.convnet.add(Dense(units = 400, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 300, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 200, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 100, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 3, kernel_initializer = 'uniform', activation = 'relu'))
-        elif network_id == 'STA_3' or network_id == 'STA_A1':
-            self.convnet.add(Dense(units = 514, kernel_initializer = 'uniform', activation = 'relu', input_shape = input_shape))
-            self.convnet.add(Dense(units = 428, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 342, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 257, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 171, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 86, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 3, kernel_initializer = 'uniform', activation = 'relu'))
-        elif network_id == 'STA_4' or network_id == 'STA_A2':
-            self.convnet.add(Dense(units = 525, kernel_initializer = 'uniform', activation = 'relu', input_shape = input_shape))
-            self.convnet.add(Dense(units = 450, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 375, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 300, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 225, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 150, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 75, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 3, kernel_initializer = 'uniform', activation = 'relu')) 
-        elif network_id == 'STA_5' or network_id == 'STA_A3':        
-            self.convnet.add(Dense(units = 533, kernel_initializer = 'uniform', activation = 'relu', input_shape = input_shape))
-            self.convnet.add(Dense(units = 467, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 400, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 333, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 267, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 200, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 133, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 65, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 3, kernel_initializer = 'uniform', activation = 'relu'))
-        elif network_id == 'SCADA_0':
-            self.convnet.add(Dense(units = 20, kernel_initializer = 'uniform', activation = 'relu', input_shape = input_shape))
-            self.convnet.add(Dense(units = 14, kernel_initializer = 'uniform', activation = 'relu'))
-        elif network_id == 'SCADA_1':
-            self.convnet.add(Dense(units = 20, kernel_initializer = 'uniform', activation = 'relu', input_shape = input_shape))
-            self.convnet.add(Dense(units = 10, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 14, kernel_initializer = 'uniform', activation = 'relu'))
-        elif network_id == 'SCADA_2':
-            self.convnet.add(Dense(units = 20, kernel_initializer = 'uniform', activation = 'relu', input_shape = input_shape))
-            self.convnet.add(Dense(units = 15, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 10, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 14, kernel_initializer = 'uniform', activation = 'relu'))
-        elif network_id == 'SCADA_3':
-            self.convnet.add(Dense(units = 20, kernel_initializer = 'uniform', activation = 'relu', input_shape = input_shape))
-            self.convnet.add(Dense(units = 15, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 10, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 5, kernel_initializer = 'uniform', activation = 'relu'))
-            self.convnet.add(Dense(units = 14, kernel_initializer = 'uniform', activation = 'relu'))
+        dropout_1 = 0.1
+        dropout_2 = 0.1
+        dropout_3 = 0.1       
+        lr = 0.001
+        
+        if network_id == 'kdd_new_archi_dropout_gradient':
+            dropout_1 = 0.3
+            dropout_2 = 0.2
+        elif network_id == 'kdd_new_archi_dropout_less':
+            dropout_1 = 0.05
+            dropout_2 = 0.05
+            dropout_3 = 0.05
+        elif network_id == 'kdd_new_archi_lr_0_0001':
+            lr = 0.0001
+        elif network_id == 'kdd_new_archi_lr_0_0006':
+            lr = 0.0006
             
+        self.convnet.add(Dense(units = 98, kernel_regularizer=l2(1e-2), kernel_initializer = 'uniform', activation = 'relu', input_shape = input_shape))
+        self.convnet.add(Dropout(dropout_1))
+        self.convnet.add(Dense(units = 79, kernel_regularizer=l2(1e-2),kernel_initializer =  'uniform',  activation = 'relu'))
+        self.convnet.add(Dropout(dropout_2))
+        self.convnet.add(Dense(units = 59, kernel_regularizer=l2(1e-2),kernel_initializer =  'uniform',  activation = 'relu'))
+        self.convnet.add(Dropout(dropout_3))
+        self.convnet.add(Dense(units = 39, kernel_regularizer=l2(1e-2),kernel_initializer =  'uniform',  activation = 'relu'))
+        self.convnet.add(Dropout(dropout_3))
+        self.convnet.add(Dense(units = 20, kernel_regularizer=l2(1e-2),kernel_initializer =  'uniform',  activation = 'relu'))
+        
+        
         #call the convnet Sequential model on each of the input tensors so params will be shared
         self.encoded_l = self.convnet(self.left_input)
         self.encoded_r = self.convnet(self.right_input)
         
         #layer to merge two encoded inputs with the l1 distance between them
-        self.L1_layer = Lambda(lambda tensors:K.abs(tensors[0] - tensors[1]))
-        
+        #self.L1_layer = Lambda(lambda tensors:K.abs(tensors[0] - tensors[1]))
+        self.L1_layer = Lambda(self.euclidean_distance, output_shape=self.eucl_dist_output_shape)
         #call this layer on list of two input tensors.
         self.L1_distance = self.L1_layer([self.encoded_l, self.encoded_r])
-        self.prediction = Dense(1,activation='sigmoid')(self.L1_distance)
-        self.siamese_net = Model(inputs=[self.left_input,self.right_input],outputs=self.prediction)
-
-        self.optimizer = Adam(0.00006)
-        self.siamese_net.compile(loss="binary_crossentropy",optimizer=self.optimizer)
-        self.siamese_net.count_params()
+       # self.prediction = Dense(1,activation='sigmoid')(self.L1_distance)
+        self.siamese_net = Model(inputs=[self.left_input,self.right_input],outputs=self.L1_distance)
+        
+        #self.optimizer = Adam(0.00006)
+        self.optimizer = Adam(lr)
+        #self.optimizer = RMSprop()
+        self.siamese_net.compile(loss="binary_crossentropy", optimizer=self.optimizer)#metrics
+        #metrics=['binary_accuracy'],
+        #self.siamese_net.summary()
+        #self.siamese_net.count_params()
         if verbose:
             print('Siamese Network Created\n')
+            
+    def b_init(self, shape,name=None):
+        """Initialize bias as in paper"""
+        values=rng.normal(loc=0.5,scale=1e-2,size=shape)
+        return K.variable(values,name=name)
+    
+    def W_init(self, shape,name=None):
+        """Initialize weights as in paper"""
+        values = rng.normal(loc=0,scale=1e-2,size=shape)
+        return K.variable(values,name=name)
+    
+    def euclidean_distance(self, vects):
+        x, y = vects
+        sum_square = K.sum(K.square(x - y), axis=1, keepdims=True)
+        return K.sqrt(K.maximum(sum_square, K.epsilon()))
+    
+    def eucl_dist_output_shape(self, shapes):
+        shape1, shape2 = shapes
+        return (shape1[0], 1)
